227cd96fa91a524cf0a6cc256a27f085c50b6364
ssowers2 <sabriyasowers@gmail.com>
2023-08-06 23:51:17 -0500

    Cleaned up white spaces in Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,26 +5,25 @@ const Table = ({ sat }) => {
   return (
     <table>
       <thead>
-        <tr>
-          <th>Name</th>
-          <th>Type of Satellite</th>
-          <th>Launch Date</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody> {/*renders the data based on button clicked*/}
-        {sat.map(sat => (
-        <tr key={sat.id}>
-        <td>{sat.name}</td>
-        <td>{sat.type}</td>
-        <td>{sat.launchDate}</td>
-        <td>
-            {sat.operational ? 'Active' : 'InActive'}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+      <tr>
+      <th>Name</th>
+      <th>Type of Satellite</th>
+      <th>Launch Date</th>
+      <th>Status</th>
+    </tr>
+  </thead>
+  <tbody>{sat.map(sat => ( //renders data based on button clicked
+  <tr key={sat.id}>
+  <td>{sat.name}</td>
+  <td>{sat.type}</td>
+  <td>{sat.launchDate}</td>
+  <td>
+    {sat.operational ? 'Active' : 'InActive'}
+  </td>
+</tr>
+))}
+  </tbody>
+</table>
   );
 };
 
