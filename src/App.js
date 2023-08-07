292a928e0b67ade0c34c4705dbc98dda2ef3d56c
ssowers2 <sabriyasowers@gmail.com>
2023-08-06 19:17:10 -0500

    Debugging the issue re: satData

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import satData from "./components/satData";
 function App() {
   const [sat, setSat] = useState(satData); //variable to manage the state of project
 
-  const displaySats = [...new Set(satData.map((data) => data.orbitType))]; // variable to hold the unique values of the orbitType property of the satellite objects
+  let displaySats = [...new Set(satData.map((data) => data.orbitType))]; // variable to hold the unique values of the orbitType property of the satellite objects
 
-  const filterByType = (currentType) => { //function that filters through the satellites
+  let filterByType = (currentType) => { //function that filters through the satellites
     const displaySats = satData.filter((newSatDisplay) => {
        return newSatDisplay.orbitType === currentType;
     });
@@ -17,7 +17,7 @@ function App() {
     setSat(displaySats);
  };
 
-return ( //The above variables and function will be used by the below buttons and table componenets
+return ( //The above variables and function will be used by the below buttons and table components
   <div>
    <>
    <Banner />
